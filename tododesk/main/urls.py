2fabce6aec12425f42ea85abc16d339c2b3b9845
Utoben <86472697+Utoben@users.noreply.github.com>
2023-02-27 18:35:55 +0500

    Файлы перемещены назад

--- tododesk/main/urls.py
+++ tododesk/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     
     path('', views.index,), #name='home'),
     path('about', views.about, name='about'),
+    path('calendar', views.calendar, name='calendar'),
 ]
--- tododesk/main/views.py
+++ tododesk/main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/billing.html')
+    return render(request, 'main/datatables.net.html')
 
 def about(request):
     return render(request, 'main/about.html')
+def calendar(request):
+    return render(request, 'main/calendar.html')
